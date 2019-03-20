979a7a9dbb5e4af8876c0b62ebf152d37300949b
unknown <phamgiang2811@gmail.com>
2019-03-21 01:31:35 +0700

    finnish login and sign up +bug in import.py

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session,render_template,request
+from flask import Flask, session,request,render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,21 +23,21 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def login():
-    return render_template("login.html")
+    return render_template("login.html") 
 
-@app.route("/signup")
-def signup():
-    return render_template("signup.html")
+@app.route("/sign_up")
+def sign_up():
+    return render_template("sign_up.html")
 
 @app.route("/signing_up",methods=["POST"] )
 def signing_up():
     name = request.form.get("name")
     password = request.form.get("password")
-    if name == None or password = None
-        return render_template("signup.html", message = "Username and password can not be empty")
-    elif db.execute("SELETE * FROM users WHERE name = :name",{"name":name}).fetch
-        return render_template("signup.html", message = "Username and password can not be empty")
+    if name =="" or password =="":
+        return render_template("sign_up.html", message = "Username and password can not be empty")
+    elif db.execute("SELECT * FROM users WHERE name = :name", {"name": name}).rowcount == 1:
+         return render_template("sign_up.html", message = "Username are already exist")
     db.execute("INSERT INTO users (name, password) VALUES (:name, :password)",
                     {"name": name, "password": password})
     db.commit()
-    return render_template("login.html")
+    return render_template("success.html")
